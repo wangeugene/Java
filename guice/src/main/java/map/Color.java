313e2e5168b221c081acb879b9887bf289a3da31
wangyuzhen <sail456852@hotmail.com>
2022-03-20 12:14:50 +0800

    add guice gradle module via IDE

---
+++ guice/src/main/java/map/Color.java
@@ -0,0 +1,30 @@
+package map;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.<br/>
+ *
+ * @author: Eugene_Wang<br />
+ * Date: 3/20/2022<br/>
+ * Time: 11:43 AM<br/>
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class Color {
+    String name;
+    int r;
+    int g;
+    int b;
+
+    public Color(String name, int r, int g, int b) {
+        this.name = name;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ guice/src/main/java/map/MapUniqueKey.java
@@ -0,0 +1,30 @@
+package map;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimaps;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.<br/>
+ *
+ * @author: Eugene_Wang<br />
+ * Date: 3/20/2022<br/>
+ * Time: 11:42 AM<br/>
+ * To change this template use File | Settings | File Templates.
+ */
+public class MapUniqueKey {
+    public static void main(String[] args) {
+        List<Color> colors = new ArrayList<>();
+        colors.add(new Color("red", 255, 0, 0));
+        colors.add(new Color("green", 0, 255, 0));
+
+        ImmutableMap<String, Color> stringColorImmutableMap = Maps.uniqueIndex(colors, Color::getName);
+        for (Map.Entry<String, Color> entry : stringColorImmutableMap.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+}
