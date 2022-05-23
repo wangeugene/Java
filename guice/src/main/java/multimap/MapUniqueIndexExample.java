cd52cd95e70c69ebd430a29259c202d25aada05e
wangyuzhen <sail456852@hotmail.com>
2022-05-23 14:33:15 +0800

    Google map usage example

--- guice/src/main/java/multimap/MapUniqueIndexExample.java
+++ guice/src/main/java/multimap/MapUniqueIndexExample.java
@@ -5,17 +5,17 @@ import com.google.common.collect.Maps;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-public class MapUniqueKey {
+/**
+ * derive key directly from value object
+ */
+public class MapUniqueIndexExample {
     public static void main(String[] args) {
         List<Color> colors = new ArrayList<>();
         colors.add(new Color("red", 255, 0, 0));
         colors.add(new Color("green", 0, 255, 0));
 
-        ImmutableMap<String, Color> stringColorImmutableMap = Maps.uniqueIndex(colors, Color::getName);
-        for (Map.Entry<String, Color> entry : stringColorImmutableMap.entrySet()) {
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-        }
+        ImmutableMap<String, Color> map = Maps.uniqueIndex(colors, Color::getName);
+        System.out.println("map = " + map);
     }
 }
