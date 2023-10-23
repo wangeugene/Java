68a5acdd999a7d5256db3521ce2f89d4b2c43575
eugene <sail456852@icloud.com>
2023-10-23 11:45:48 +0800

    Singleton

---
+++ app/src/main/java/loading/ClassLoader.java
@@ -0,0 +1,47 @@
+package loading;
+
+class SuperClassLoader {
+    public SuperClassLoader() {
+        System.out.println("SuperClass Constructor 5");
+    }
+
+    static {
+        System.out.println("SuperClass Static Initialize Block 1");
+    }
+
+    {
+        System.out.println("SuperClass Initialize Block 4");
+    }
+}
+
+public class ClassLoader extends SuperClassLoader {
+    {
+        System.out.println("Initialize Block 6");
+    }
+    public ClassLoader() {
+        System.out.println("Constructor 7");
+    }
+
+    static {
+        System.out.println("Static Initialize Block 2");
+    }
+}
+
+class SubClassLoader extends ClassLoader {
+    public static void main(String[] args) {
+        new SubClassLoader();
+    }
+
+    public SubClassLoader() {
+        System.out.println("SubClass Constructor 9");
+    }
+
+    static {
+        System.out.println("SubClass Static Initialize Block 3");
+    }
+
+    {
+        System.out.println("SubClass Initialize Block 8");
+    }
+}
+
---
+++ designPatterns/src/main/java/singleton/Singleton.java
@@ -0,0 +1,24 @@
+package singleton;
+
+public class Singleton {
+    private Singleton() {
+    }
+    private volatile static Singleton singleton = new Singleton();
+
+    public static Singleton getSingleton() {
+        if(singleton == null){
+            synchronized (Singleton.class){
+                if(singleton == null){
+                    singleton = new Singleton();
+                }
+                return singleton;
+            }
+        }else{
+            return singleton;
+        }
+    }
+
+    protected void SayMyName(){
+        System.out.println("My name is Singleton");
+    }
+}
