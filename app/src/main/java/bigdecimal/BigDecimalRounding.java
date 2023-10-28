39923058cbfe0148382a605320fd2efb4f007e8b
eugene <sail456852@icloud.com>
2023-10-28 14:30:06 +0800

    Fake

--- app/src/main/java/bigdecimal/BigDecimalRounding.java
+++ app/src/main/java/bigdecimal/BigDecimalRounding.java
@@ -3,16 +3,13 @@ package bigdecimal;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-public class SetScales {
+public class BigDecimalRounding {
 
-    private static BigDecimal numerator;
-    private static BigDecimal denominator;
-    private static int scale;
+    private static BigDecimal numerator = BigDecimal.TEN;
+    private static BigDecimal denominator = BigDecimal.valueOf(3);
+    private static int scale = 5;
 
     public static void main(String[] args) {
-        numerator = BigDecimal.TEN;
-        denominator = BigDecimal.valueOf(3);
-        scale = 5;
         divideWithHalfUpMode();
         divideWithUpMode();
         divideWithHalfUpModeAndScale();
@@ -21,19 +18,15 @@ public class SetScales {
 
     private static void divideWithUpMode() {
         BigDecimal quotientUp = numerator.divide(denominator, RoundingMode.UP);
-        System.out.println("quotientUp = " + quotientUp);
     }
 
     private static void divideWithHalfUpMode() {
         BigDecimal quotientHalfUp = numerator.divide(denominator, RoundingMode.HALF_UP);
-        System.out.println("quotientHalfUp = " + quotientHalfUp);
     }
     private static void divideWithHalfUpModeAndScale() {
         BigDecimal quotientWithScaleHalfUp = numerator.divide(denominator, scale, RoundingMode.HALF_UP);
-        System.out.println("quotientWithScaleHalfUp = " + quotientWithScaleHalfUp);
     }
     private static void divideWithUpModeAndScale() {
         BigDecimal quotientWithScaleUp = numerator.divide(denominator, scale, RoundingMode.UP);
-        System.out.println("quotientWithScaleUp = " + quotientWithScaleUp);
     }
 }
---
+++ app/src/main/java/interfaces/DeepCloneClonable.java
@@ -0,0 +1,81 @@
+package interfaces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeepCloneClonable {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setAge(31);
+        user.setName("WangWangWangWangWangWangWangWangWangWangWangWangWangWangWangWangWangWangWangWangWangWangWangWangWangWangWangWangWangWangWang");
+        ArrayList<String> experiences = new ArrayList<>();
+        experiences.add("PingAn");
+        experiences.add("LeShua");
+        experiences.add("EPAM");
+        user.setExperiences(experiences);
+
+        User cloned = user.clone();
+
+        System.out.println(user.getName());
+        System.out.println(user.getAge());
+        System.out.println(cloned.getName());
+        System.out.println(cloned.getAge());
+        System.out.println(cloned.getExperiences());
+
+        cloned.setAge(33);
+        cloned.setName("Li");
+        cloned.getExperiences().set(1, "LeShuaFake");
+
+        System.out.println(user.getName());
+        System.out.println(user.getAge());
+        System.out.println(cloned.getName());
+        System.out.println(cloned.getAge());
+        System.out.println(user.hashCode());
+        System.out.println(cloned.hashCode());
+        System.out.println(user.getExperiences());
+        System.out.println(cloned.getExperiences());
+    }
+}
+
+class User implements Cloneable{
+    private String name;
+    private int age;
+    private List<String> experiences;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public User clone() {
+        try {
+            // TODO: copy mutable state here, so the clone can't change the internals of the original
+            User cloned = (User) super.clone();
+            cloned.experiences = new ArrayList<>(experiences);
+            return cloned;
+
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getExperiences() {
+        return experiences;
+    }
+
+    public void setExperiences(List<String> experiences) {
+        this.experiences = experiences;
+    }
+}
