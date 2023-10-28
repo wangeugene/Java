93a5934bc66de6474a25eb05481780cf263be77b
eugene <sail456852@icloud.com>
2023-10-28 21:44:40 +0800

    CO

---
+++ designPatterns/src/main/java/adapter/AdapterClient.java
@@ -0,0 +1,10 @@
+package adapter;
+
+public class AdapterClient {
+    private static Adaptor adaptor;
+
+    public static void main(String[] args) {
+        adaptor = new AdaptorImpl(new AdapteeImpl());
+        adaptor.fitClient();
+    }
+}
