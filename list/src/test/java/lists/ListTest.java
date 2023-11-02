127c0f26ce0572c960776036dbdf45807da75f3c
eugene <sail456852@icloud.com>
2023-11-02 17:32:00 +0800

    Java.list package name caused test unable to run

---
+++ list/src/test/java/lists/ListTest.java
@@ -0,0 +1,20 @@
+package lists;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+class ListTest {
+    @Test
+    void removeFromFirst() {
+        AtomicReference<List<Integer>> list = new AtomicReference<>(new ArrayList<>());
+        list.get().add(1);
+        list.get().add(2);
+        list.get().add(3);
+        list.get().add(4);
+        list.get().remove(1);
+        System.out.println(list);
+    }
+}
