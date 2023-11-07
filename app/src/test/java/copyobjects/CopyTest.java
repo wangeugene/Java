708198c330f7a628c15318981e866843c372c771
eugene <sail456852@icloud.com>
2023-11-07 15:58:59 +0800

    Deep Copy & Shallow Copy

---
+++ app/src/test/java/copyobjects/CopyTest.java
@@ -0,0 +1,47 @@
+package copyobjects;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static copyobjects.Copy.user;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+final class CopyTest {
+
+    private static User shallowCopy;
+    private static User deepCopy;
+
+    @BeforeAll
+    static void setUp() {
+        shallowCopy = ShallowCopy.getShallowCopy();
+        deepCopy = DeepCopy.getDeepCopy();
+        mutateExperiencesListInOriginalUser();
+    }
+
+    private static void mutateExperiencesListInOriginalUser() {
+        List<String> experiences = user.getExperiences();
+        experiences.set(0, "PingAnChanged");
+        user.setExperiences(experiences);
+    }
+
+    @Test
+    void getShallowCopy() {
+        commonAssertions(shallowCopy);
+        assertEquals("PingAnChanged", shallowCopy.getExperiences().get(0));
+    }
+
+    @Test
+    void getDeepCopy() {
+        commonAssertions(deepCopy);
+        assertEquals("PingAn", deepCopy.getExperiences().get(0));
+    }
+
+    private static void commonAssertions(User shallowCopy) {
+        assertEquals("Wang", shallowCopy.getName());
+        assertEquals(31, shallowCopy.getAge());
+        assertNotNull(shallowCopy.getExperiences());
+    }
+}
