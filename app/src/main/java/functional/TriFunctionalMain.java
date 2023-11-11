f9c981fed49a376493efac090ac36e6b3f8dafd2
eugene <sail456852@icloud.com>
2023-11-11 20:22:35 +0800

    Customized TriFunctional Interface

---
+++ app/src/main/java/functional/TriFunctionalMain.java
@@ -0,0 +1,11 @@
+package functional;
+
+public class TriFunctionalMain {
+    public static void main(String[] args) {
+        TriFunctional<Integer, Integer, Integer, Integer> triFunc =
+                (x, y, z) -> x * y + z;
+        Integer y = triFunc.apply(1, 5, 7);
+        // y = 1 * 5 + 7 = 12
+        System.out.println("y = " + y);
+    }
+}
