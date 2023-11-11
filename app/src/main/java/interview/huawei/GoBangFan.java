2a33fa0f9f9acbf2ca17a9614d4b872b4e7d23b0
eugene <sail456852@icloud.com>
2023-11-11 19:45:36 +0800

    GoBang OD finished

--- app/src/main/java/interview/huawei/GoBangFan.java
+++ app/src/main/java/interview/huawei/GoBangFan.java
@@ -4,6 +4,8 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static java.util.Map.*;
+
 /**
  * <a href="https://zhuanlan.zhihu.com/p/662299546">Original Test Question</a>
  */
@@ -20,11 +22,27 @@ public class GoBangFan {
         Long count = countZero.apply(simpleOne);
         System.out.println("count = " + count);
 
-        HashMap<Integer, Integer> indexMaxSpan = getIndexMaxSpanForEachIndex(simpleOne, 1);
+        HashMap<Integer, Integer> indexMaxSpan = getIndexMaxSpanForEachIndex();
         System.out.println("indexMaxSpan = " + indexMaxSpan);
 
         int index = getTheMostSuitableIndexToDropPawn(indexMaxSpan);
         System.out.println("index = " + index);
+    }
+
+    /**
+     * @param indexMaxSpan which means there are equal case when we should put the pawn in the one index that most near the mid-point of the pawn line
+     * @return the nearest index position when there are many equal span cases.
+     */
+    private static int getIndexNearToTheMiddle(TreeMap<Integer, List<Integer>> indexMaxSpan) {
+        final int midPoint = (int) (0.5 * (GoBangFan.simpleOne.length - 1));
+        Entry<Integer, List<Integer>> integerListEntry = indexMaxSpan
+                .entrySet()
+                .stream()
+                .min(
+                        Comparator.comparingInt(entry -> Math.abs(entry.getKey() - midPoint))
+                )
+                .orElseThrow();
+        return integerListEntry.getKey();
     }
 
     private static int getTheMostSuitableIndexToDropPawn(HashMap<Integer, Integer> indexMaxSpan) {
@@ -32,15 +50,16 @@ public class GoBangFan {
         if (duplicatedMaxSpanIndexes != null && !duplicatedMaxSpanIndexes.isEmpty()) {
             Integer maxSpan = duplicatedMaxSpanIndexes.lastKey();
             List<Integer> indexes = duplicatedMaxSpanIndexes.get(maxSpan);
-            return indexes.stream().mapToInt(Integer::intValue).max().orElse(0);
-        }else{
-            Optional<Map.Entry<Integer, Integer>> max = indexMaxSpan.entrySet()
+            int midIndex = getIndexNearToTheMiddle(duplicatedMaxSpanIndexes);
+            return indexes.stream().mapToInt(Integer::intValue).max().orElse(midIndex);
+        } else {
+            Optional<Map.Entry<Integer, Integer>> maxEntry = indexMaxSpan.entrySet()
                     .stream()
-                    .max(Map.Entry.comparingByValue());
-            if(max.isPresent()){
-                Map.Entry<Integer, Integer> entry = max.get();
+                    .max(Entry.comparingByValue());
+            if (maxEntry.isPresent()) {
+                Map.Entry<Integer, Integer> entry = maxEntry.get();
                 return entry.getKey();
-            }else{
+            } else {
                 return -1;
             }
         }
@@ -50,35 +69,35 @@ public class GoBangFan {
         return indexMaxSpan.entrySet()
                 .stream()
                 .collect(Collectors.groupingBy(
-                        Map.Entry::getValue,
-                        Collectors.mapping(Map.Entry::getKey, Collectors.toList())
+                        Entry::getValue,
+                        Collectors.mapping(Entry::getKey, Collectors.toList())
                 ))
                 .entrySet()
                 .stream()
                 .filter(entry -> entry.getValue().size() > 1)
                 .collect(
                         Collectors.toMap(
-                                Map.Entry::getKey,
-                                Map.Entry::getValue,
+                                Entry::getKey,
+                                Entry::getValue,
                                 (o, n) -> o,
                                 TreeMap::new
                         )
                 );
     }
 
-    private static HashMap<Integer, Integer> getIndexMaxSpanForEachIndex(int[] line, int currentPawn) {
+    private static HashMap<Integer, Integer> getIndexMaxSpanForEachIndex() {
         HashMap<Integer, Integer> indexByMaxSpan = new HashMap<>();
-        for (int i = 0; i < line.length; i++) {
-            if (line[i] == 0) {
+        for (int i = 0; i < GoBangFan.simpleOne.length; i++) {
+            if (GoBangFan.simpleOne[i] == 0) {
                 int leftCount = 0;
                 int leftCur = i - 1;
-                while (leftCur >= 0 && line[leftCur] == currentPawn) {
+                while (leftCur >= 0 && GoBangFan.simpleOne[leftCur] == 1) {
                     leftCur--;
                     leftCount++;
                 }
                 int rightCount = 0;
                 int rightCur = i + 1;
-                while (rightCur < line.length && line[rightCur] == currentPawn) {
+                while (rightCur < GoBangFan.simpleOne.length && GoBangFan.simpleOne[rightCur] == 1) {
                     rightCur++;
                     rightCount++;
                 }
