ef3eba0cd96a44d760e83cfa0e776bf569aac44c
eugene <sail456852@icloud.com>
2023-11-11 20:14:23 +0800

    Hello

---
+++ app/src/main/java/functional/FunctionalComposition.java
@@ -0,0 +1,16 @@
+package functional;
+
+import java.util.function.Function;
+
+public class FunctionalComposition {
+    public static void main(String[] args) {
+        Function<Integer, Integer> times3 = x -> x * 3;
+        Function<Integer, Integer> plus1 = x -> x + 1;
+        // y = 3x + 1 = 16;
+        Integer y = times3.andThen(plus1).apply(5);
+        System.out.println("y = " + y);
+        /// y = ( x + 1 ) * 3 = 18
+        Integer z = plus1.andThen(times3).apply(5);
+        System.out.println("z = " + z);
+    }
+}
