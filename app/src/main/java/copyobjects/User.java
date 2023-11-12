81a3bb050b8517c42d335fb18d6e164975a9a8ab
eugene <sail456852@icloud.com>
2023-11-12 11:01:52 +0800

    Map Streaming

--- app/src/main/java/copyobjects/User.java
+++ app/src/main/java/copyobjects/User.java
@@ -1,11 +1,17 @@
 package copyobjects;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User implements Cloneable {
 
     private String name;
