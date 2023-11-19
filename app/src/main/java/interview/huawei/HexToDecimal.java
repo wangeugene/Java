a719a0f4502943ef2eeea73be30e31f128fe8754
eugene <sail456852@icloud.com>
2023-11-19 22:31:23 +0800

    IntStream generation

--- app/src/main/java/interview/huawei/HexToDecimal.java
+++ app/src/main/java/interview/huawei/HexToDecimal.java
@@ -4,5 +4,6 @@ public class HexToDecimal {
     public static void main(String[] args) {
         String hex = "AA";
         System.out.println(Integer.parseInt(hex, 16));
+        System.out.printf("15%s", hex);
     }
 }
