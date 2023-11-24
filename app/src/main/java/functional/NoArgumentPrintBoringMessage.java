a2852c214fa69f41ce61f2e65fec218df3557946
eugene <sail456852@icloud.com>
2023-11-24 16:57:10 +0800

    rename files

--- app/src/main/java/functional/NoArgumentPrintBoringMessage.java
+++ app/src/main/java/functional/NoArgumentPrintBoringMessage.java
@@ -2,9 +2,7 @@ package functional;
 
 public class NoArgumentPrintBoringMessage {
     public static void main(String[] args) {
-        NoArgumentFunctional noArgumentFunc = () -> {
-            System.out.println("No Argument Functional");
-        };
+        NoArgumentFunctional noArgumentFunc = () -> System.out.println("No Argument Functional");
         noArgumentFunc.apply();
     }
 }
