8e133546f84d979c7721c56393cd5869a3d99a73
eugene <sail456852@icloud.com>
2023-12-26 14:22:47 +0800

    Synchronized Block vs Reentrant Lock Peformance Comparison

---
+++ NodeJS/create_object.js
@@ -0,0 +1,30 @@
+// new Rectangular Object
+const create_rectangle = (length, width) => {
+    this.length = length;
+    this.width = width;
+    this.area = () => {
+        return this.length * this.width;
+    };
+    this.perimeter = () => {
+        return 2 * (this.length + this.width);
+    };
+    return this;
+}
+
+// another way to create an object which I think is better
+function Rectangle(a, b) {
+    this.length = a;
+    this.width = b;
+    this.area = this.length * this.width
+    this.perimeter = 2 * (this.length + this.width)
+}
+
+aRectangle = create_rectangle(2, 3)
+console.log(aRectangle.area());
+console.log(aRectangle.perimeter());
+
+const rec = new Rectangle(5,6)
+console.log(rec.length)
+console.log(rec.width)
+console.log(rec.area)
+console.log(rec.perimeter)
---
+++ Swift/Swift.md
@@ -0,0 +1,10 @@
+Swift is a fantastic way to write software for phones, tables, desktops, servers, or anything
+else that runs code.It's a safe and fast language that combines the best in modern language thinking with wisdom from
+a diverse open source community.
+
+* Variables are always initialized before use.
+* Array indices are checked for out-of-bounds errors.
+* Integers are checked for overflow.
+* Optionals ensure that nil values are handled explicitly.
+* Memory is managed automatically.
+* Error handling allows controlled recovery from unexpected failures.
