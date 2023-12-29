2a88d7ef48f897c878b107b9c4fc29d2eeb4beea
eugene <sail456852@icloud.com>
2023-12-29 14:58:28 +0800

    clean up mockito

---
+++ app/src/test/java/mockito/SpyTest.java
@@ -0,0 +1,24 @@
+package mockito;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class SpyTest {
+    @Spy
+    List<String> spyList;
+
+    @Test
+    void whenNotUseSpyAnnotationThenCorrect() {
+        spyList.add("one");
+        spyList.add("two");
+
+        Mockito.verify(spyList).add("one");
+        Mockito.verify(spyList).add("two");
+    }
+}
