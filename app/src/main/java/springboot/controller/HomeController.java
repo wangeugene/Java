dc1c857b68b52b4f5004a30ecd4ec7bba2d9a9a7
eugene <sail456852@icloud.com>
2024-02-18 16:28:05 +0800

    feat: Add SpringBoot

---
+++ app/src/main/java/springboot/controller/HomeController.java
@@ -0,0 +1,12 @@
+package springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Home!";
+    }
+}
