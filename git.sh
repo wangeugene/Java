50e32cdf1a6d6c17041e6b8a4bcd602817912c8f
eugene <sail456852@icloud.com>
2024-06-06 11:27:03 +0800

    sh: git push

--- git.sh
+++ git.sh
@@ -2,11 +2,8 @@
 if ! pgrep ssh-agent;
 then
   echo "ssh-agent is not running"
-  "eval $(ssh-agent -s)"
-  ssh-add /Users/eugene/.ssh/id_ed25519_eugene
-  ssh-add -l
 else
   echo "ssh-agent is already running"
 fi
 
-git push
+GIT_SSH_COMMAND="ssh -i /Users/eugene/.ssh/id_ed25519_eugene" git push origin main
