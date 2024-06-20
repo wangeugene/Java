4e7d501f49992907b9cfaed57bf6ae43a256d188
eugene <sail456852@icloud.com>
2024-06-20 10:30:44 +0800

    shell: environment variable

--- Shell/update_env_var_to_shell_config_file.sh
+++ Shell/update_env_var_to_shell_config_file.sh
@@ -5,3 +5,7 @@ sed -i '' '/export ENV_VAR=".*"/d' ~/.zshrc
 # make this environment variable available to all new shell sessions
 echo 'export ENV_VAR="Hello World"' >> ~/.zshrc
 source ~/.zshrc
+
+echo Environment Variable ENV_VAR=$ENV_VAR has been set
+
+echo $(cat ~/.zshrc | grep 'export ENV_VAR')
