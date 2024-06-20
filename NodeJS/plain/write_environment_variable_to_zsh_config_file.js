d76d2ec3e6b54e15d5ad74ddc7b8a1d7bfcae761
eugene <sail456852@icloud.com>
2024-06-20 11:56:14 +0800

    clean: tidy up

---
+++ NodeJS/plain/write_environment_variable_to_zsh_config_file.js
@@ -0,0 +1,10 @@
+// export TOKEN='exampleToken' >> ~/.zshrc
+// write code to write environment variable to ~/.zshrc file
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
+// write string: export Token='exampleTokne' to ~/.zshrc file
+fs.appendFileSync(path.join(os.homedir(), '.zshrc'), "export TOKEN='exampleToken'\n");
+
+// print out the content of ~/.zshrc file
+console.log(fs.readFileSync(path.join(os.homedir(), '.zshrc'), 'utf8'))
