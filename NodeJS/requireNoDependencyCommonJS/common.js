2659a3a53872073dac0288b6ff364dbaef94e0ad
eugene <sail456852@icloud.com>
2024-06-27 15:09:00 +0800

    clean: categorize JS

--- NodeJS/requireNoDependencyCommonJS/common.js
+++ NodeJS/requireNoDependencyCommonJS/common.js
@@ -1,3 +1,3 @@
-const path = require('path')
+const path = require('NodeJS/requireNoDependencyCommonJS/path')
 console.log(__dirname)
 console.log(`The absolute path is: ${path.resolve(__dirname, "package.json")}`)
