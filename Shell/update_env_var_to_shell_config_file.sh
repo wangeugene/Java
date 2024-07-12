6463dc68bd66956f5c824d5ba7021a45d1bbc7d5
eugene <sail456852@icloud.com>
2024-07-12 10:48:30 +0800

    sh: update delete lines matching regular expression

--- Shell/update_env_var_to_shell_config_file.sh
+++ Shell/update_env_var_to_shell_config_file.sh
@@ -1,6 +1,9 @@
-# remove lines matching 'export ENV_VAR="Hello World"' in ~/.zshrc file
 
-sed -i '' '/export ENV_VAR=".*"/d' ~/.zshrc
+# delete lines containing ENV_VAR from the shell config file
+# sed is a stream editor used to perform text transformations
+# -i '' edit the file in-place, without creating a backup
+# '/ENV_VAR=.*/d' is a sed expression that matches lines containing ENV_VAR= and deletes them.
+sed -i '' '/ENV_VAR=.*/d' ~/.zshrc
 
 # make this environment variable available to all new shell sessions
 echo 'export ENV_VAR="Hello World"' >> ~/.zshrc
