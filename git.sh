e8fd4226e036ca5537c98a3f74bad28aea37d8f6
eugene <sail456852@icloud.com>
2024-09-03 12:52:00 +0800

    ssh: dual github account pushing

--- git.sh
+++ git.sh
@@ -6,4 +6,7 @@ else
   echo "ssh-agent is already running"
 fi
 
+# add the key to the agent first, or else the git push will fail with a permission denied error
+# first execute the following command to add the key to the agent interactively ( cause you need to enter the passphrase)
+# ssh-add /Users/eugene/.ssh/id_ed25519_eugene
 GIT_SSH_COMMAND="ssh -i /Users/eugene/.ssh/id_ed25519_eugene" git push origin main
