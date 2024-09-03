dfd55573f13550fa3242d78243eba09b5dcad29f
eugene <sail456852@icloud.com>
2024-09-03 12:44:29 +0800

    ssh: MacOS ssh-agent worked

--- Shell/ssh_agent.sh
+++ Shell/ssh_agent.sh
@@ -1,4 +1,6 @@
 # shellcheck disable=SC2046
 eval `ssh-agent -s`
-ssh-add /Users/eugene/.ssh/id_ed25519
+# MacOS specific way of adding keys to the agent, this will avoid the need to enter the passphrase every time
+# Without --apple-use-keychain, the passphrase will be asked every time
+ssh-add --apple-use-keychain ~/.ssh/id_ed25519
 ssh-add -l
