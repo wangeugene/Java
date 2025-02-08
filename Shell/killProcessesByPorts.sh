7a23a09f0b101583fc0f405ab39e739b81f606ba
Eugene Wang <yuzhen23@icloud.com>
2025-02-08 17:12:17 +0800

    feat: update .zshrc

--- Shell/killProcessesByPorts.sh
+++ Shell/killProcessesByPorts.sh
@@ -1,8 +1,9 @@
 # Usage: sh killProcessesByPorts.sh
+# alias kp='sh ~/Projects/Java/Shell/killProcessesByPorts.sh'
 
 # take an array of ports to kill processes by arguments from command line
 ARG_PORTS=("$@")
-PORTS=(8080 9229 3000)
+PORTS=(8080 9229)
 
 # append ARG_PORTS to the PORTS array
 for PORT in "${ARG_PORTS[@]}"; do
@@ -20,10 +21,24 @@ for PORT in "${PORTS[@]}"; do
     fi
 done
 
-NPID=$(lsof -i:443 | grep node | awk '{print $2}')
-if [ -n "$NPID" ]; then
-    kill -9 $NPID
-    echo "Killed nodejs process $NPID on port 443"
-else
-    echo "No nodejs process found on port 443"
-fi
+# Killing all the cypress process
+CY_PIDs=$(ps aux | grep cypress | awk '{print $2}')
+for CY_PID in $CY_PIDs; do
+    kill -9 $CY_PID
+    echo "Killed cypress process of pid: $CY_PID"
+done
+
+GRADLE_PIDs=$(ps aux | grep gradle | awk '{print $2}')
+for GRADLE_PID in $GRADLE_PIDs; do
+    kill -9 $GRADLE_PID
+    echo "Killed Gradle process of pid: $GRADLE_PID"
+done
+
+# Killing the node process on port 443
+# NPID=$(lsof -i:443 | grep node | awk '{print $2}')
+# if [ -n "$NPID" ]; then
+#     kill -9 $NPID
+#     echo "Killed nodejs process $NPID on port 443"
+# else
+#     echo "No nodejs process found on port 443"
+# fi
