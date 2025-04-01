fbfbbd2b09c8e3e35b94b723a0bf90c53dead9d2
wangeugene <yuzhen23@icloud.com>
2025-04-01 17:18:27 +0800

    feat: optimize runDocker.sh

--- Typescript/zuche/runDocker.sh
+++ Typescript/zuche/runDocker.sh
@@ -1,3 +1,6 @@
 docker build -t zuche:1.0 .
-docker rm -f zuche_container || true
+if [ "$(docker ps -aq -f name=zuche_container)" ]; then
+  docker rm -f zuche_container
+fi
 docker run --env-file .env --name zuche_container zuche:1.0
+docker logs zuche_container
