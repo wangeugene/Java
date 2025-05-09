5bfb05ef17b15b14d1eaed8d67f065a9e5d1a551
wangeugene <yuzhen23@icloud.com>
2025-05-09 16:25:44 +0800

    fix: ssh_agent.sh

--- Shell/ssh_agent.sh
+++ Shell/ssh_agent.sh
@@ -1,12 +1,37 @@
-# Add to ~/.bashrc or ~/.bash_profile
-# alias sshagent="/Users/eugene/IdeaProjects/Java/Shell/ssh_agent.sh"
-if ! pgrep -x "ssh-agent" >/dev/null; then
-    eval $(ssh-agent -s)
+# Add to ~/.zshrc
+# Check if alias already exists
+if grep -q "^alias sshagent=" ~/.zshrc; then
+    echo "alias sshagent already exists in ~/.zshrc"
 else
-    echo "ssh-agent is already running with the following PID(s):"
-    pgrep -x "ssh-agent"
+    echo "alias sshagent does not exist && adding alias sshagent to ~/.zshrc"
+    echo "alias sshagent='~/Projects/Java/Shell/ssh_agent.sh'" >> ~/.zshrc
 fi
-# MacOS specific way of adding keys to the agent, this will avoid the need to enter the passphrase every time
-# Without --apple-use-keychain, the passphrase will be asked every time
-ssh-add --apple-use-keychain ~/.ssh/id_ed25519
-ssh-add -l
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "macOS detected, using Apple Keychain"
+    # macOS
+    KEYCHAIN_FLAG="--apple-use-keychain"
+    if ! pgrep -x "ssh-agent" >/dev/null; then
+        eval $(ssh-agent -s)
+        ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+        ssh-add -l
+    else
+        echo "ssh-agent is already running with the following PID(s):"
+        pgrep -x "ssh-agent"
+    fi
+else
+    # Linux and others
+    echo "Linux detected, using default ssh-agent"
+    KEYCHAIN_FLAG=""
+    if ! pgrep -x "ssh-agent" >/dev/null; then
+        eval $(ssh-agent -s)
+        ssh-add ~/.ssh/id_ed25519
+        ssh-add -l
+    else
+        echo "ssh-agent is already running with the following PID(s):"
+        pgrep -x "ssh-agent"
+    fi
+fi
+
+# MacOS specific way of adding keys to the agent
+ssh -T git@github.com
