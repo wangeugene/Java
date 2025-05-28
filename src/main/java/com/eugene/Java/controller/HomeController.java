fd897bdb3d37f1a90fff783f02f0a5ac930fef74
Eugene Wang <yuzhen23@icloud.com>
2025-05-28 18:30:20 +0800

    feat: add integration tests & UTs

--- src/main/java/com/eugene/Java/controller/HomeController.java
+++ src/main/java/com/eugene/Java/controller/HomeController.java
@@ -11,6 +11,13 @@ public class HomeController {
         return "Hello, Home!";
     }
 
+    /**
+     * request URL: http://localhost:8080/url-encoded/wang%20eugene
+     * response: Hello, wang eugene!
+     *
+     * @param name
+     * @return
+     */
     @GetMapping("/url-encoded/{name}")
     public String urlEncoded(@PathVariable String name) {
         return "Hello, " + name + "!";
---
+++ src/test/java/com/eugene/Java/integration/HomeControllerIT.java
@@ -0,0 +1,36 @@
+package com.eugene.Java.integration;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.net.URI;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HomeControllerIT {
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * Use URL.create to pass URL-encoded string to MockMvc to mimic real-world scenarios
+     * http://localhost:8080/url-encoded/Eugene%20Wang
+     *
+     * @throws Exception
+     */
+    @Test
+    @DisplayName("Returns personalized greeting for url-encoded endpoint with special characters")
+    void returnsPersonalizedGreetingForUrlEncodedEndpointWithSpecialCharacters() throws Exception {
+        mockMvc
+                .perform(get(URI.create("/url-encoded/Eugene%20Wang")))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello, Eugene Wang!"));
+    }
+}
