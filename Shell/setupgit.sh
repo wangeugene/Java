ef0b84c7b21f74c3177ed1f1f06135bae36936e6
Eugene Wang <yuzhen23@icloud.com>
2025-05-28 15:07:29 +0800

    test: quick commit & push

--- Shell/setupgit.sh
+++ Shell/setupgit.sh
@@ -8,8 +8,8 @@ else
 fi
 
 cd ~/Projects/Java
-git config user.email "yuzhen23@icloud.com" --local
-git config user.name "Eugene Wang" --local
+git config --global --replace-all user.email "yuzhen23@icloud.com"
+git config --global --replace-all user.name "Eugene Wang"
 
 # add the key to the agent first, or else the git push will fail with a permission denied error
 # first execute the following command to add the key to the agent interactively ( cause you need to enter the passphrase)
