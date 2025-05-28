f08a88b0e82c9d4668542aa64feffafde181c38a
Eugene Wang <yuzhen23@icloud.com>
2025-05-28 17:08:42 +0800

    feat: introducing IDEA format on save automatically feature

---
+++ IDEA.md
@@ -0,0 +1,10 @@
+# IDEA Tips
+
+## To format the code on save, can't be done with .editorconfig file and its plugin
+
+	1.	Go to Preferences (⌘ ,) → Advanced Settings
+	2.	Search for “Save”
+	3.	Enable:
+	•	✅ Reformat code on save
+	•	✅ Optimize imports on save
+	•	✅ Rearrange code on save (optional) 
--- src/main/java/com/eugene/Java/controller/HomeController.java
+++ src/main/java/com/eugene/Java/controller/HomeController.java
@@ -1,12 +1,18 @@
 package com.eugene.Java.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-  @GetMapping("/")
-  public String home() {
-    return "Hello, Home!";
-  }
+    @GetMapping("/")
+    public String home() {
+        return "Hello, Home!";
+    }
+
+    @GetMapping("/url-encoded/{name}")
+    public String urlEncoded(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
 }
