37bd838d08131fd353b2bcc6b62d57a7dbd00af2
Eugene Wang <yuzhen23@icloud.com>
2025-07-02 15:32:41 +0800

    feat: a working one with simple set up

--- src/main/resources/db/changelog/changes/001-create-employee-tables.sql
+++ src/main/resources/db/changelog/changes/001-create-employee-tables.sql
@@ -1,10 +1,10 @@
 
--- Create user table on schema eugene on Azure SQL Server Database
-CREATE TABLE eugene.user
+-- Create employee table on schema eugene on Azure SQL Server Database
+CREATE TABLE eugene.employee
 (
     id                  BIGINT IDENTITY (1,1) PRIMARY KEY,
-    user_id           NVARCHAR(64) NOT NULL,
-    user_name         NVARCHAR(64) NOT NULL,
+    employee_id           NVARCHAR(64) NOT NULL,
+    employee_name         NVARCHAR(64) NOT NULL,
     city                NVARCHAR(64),
     type                SMALLINT CHECK (type IN (0, 1, 2)),
     status              SMALLINT     NOT NULL DEFAULT 0 CHECK (status IN (0, 1, 2)),
@@ -18,7 +18,7 @@ CREATE TABLE eugene.user
     account_city        NVARCHAR(64),
     company             NVARCHAR(64),
     account_number      NVARCHAR(64),
-    user_email        NVARCHAR(320),
+    employee_email      NVARCHAR(320),
     login_status        SMALLINT     NOT NULL DEFAULT 0 CHECK (login_status IN (0, 1)),
     update_time         DATETIME2    NOT NULL DEFAULT SYSDATETIME(),
     update_by           NVARCHAR(64),
@@ -26,15 +26,15 @@ CREATE TABLE eugene.user
 );
 
 -- Unique constraint & index
-ALTER TABLE eugene.user
-    ADD CONSTRAINT uq_user_phone_number UNIQUE (phone_number);
-CREATE INDEX idx_user_phone_number ON eugene.user (phone_number);
+ALTER TABLE eugene.employee
+    ADD CONSTRAINT uq_employee_phone_number UNIQUE (phone_number);
+CREATE INDEX idx_employee_phone_number ON eugene.employee (phone_number);
 
--- Create user_verification_code table
-CREATE TABLE eugene.user_verification_code
+-- Create employee_verification_code table
+CREATE TABLE eugene.employee_verification_code
 (
     id           BIGINT IDENTITY (1,1) PRIMARY KEY,
-    user_id    BIGINT        NOT NULL FOREIGN KEY REFERENCES eugene.user (id) ON DELETE CASCADE,
+    employee_id    BIGINT        NOT NULL FOREIGN KEY REFERENCES eugene.employee (id) ON DELETE CASCADE,
     phone_number NVARCHAR(32)  NOT NULL,
     content      NVARCHAR(256) NOT NULL,
     expire_time  DATETIME2     NOT NULL,
@@ -44,4 +44,4 @@ CREATE TABLE eugene.user_verification_code
 );
 
 CREATE INDEX idx_verification_phone_number
-    ON eugene.user_verification_code (phone_number);
+    ON eugene.employee_verification_code (phone_number);
