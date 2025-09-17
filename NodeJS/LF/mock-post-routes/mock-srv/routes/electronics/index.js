5e5f734fb74f64596c0c7cfdf5a5973f87fcd7d6
wangeugene <yuzhen23@icloud.com>
2025-09-17 13:47:17 +0800

    feat: mock post request but GET services not working

--- NodeJS/LF/mock-post-routes/mock-srv/routes/electronics/index.js
+++ NodeJS/LF/mock-post-routes/mock-srv/routes/electronics/index.js
@@ -1,16 +1,16 @@
 'use strict'
 
-/* Where {DATA HERE} is whatever mock data we wish to send as a response for that route.
+const data = [
+  { id: 'A1', name: 'Vacuum Cleaner', rrp: '99.99', info: 'The worst vacuum in the world.' },
+  { id: 'A2', name: 'Leaf Blower', rrp: '303.33', info: 'This product will blow your socks off.' },
+]
 
-Although we are not using them yet, it's important to note that the route handler function is also passed request and reply objects. These are conceptually the same, but functionally different to the req and res objects passed to the Node core http.createServer request listener function, because they have their own (higher level) APIs. See the Fastify Request and the Fastify Reply documentation for full information on their APIs.
-*/
-
-// This function is named: route handler function
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return [
-      { id: 'A1', name: 'Vacuum Cleaner', rrp: '99.99', info: 'The worst vacuum in the world.' },
-      { id: 'A2', name: 'Leaf Blower', rrp: '303.33', info: 'This product will blow your socks off.' },
-    ]
+    return data
+  })
+  fastify.post('/', async function (request, reply) {
+    request.mockDataInsert(opts.prefix.slice(1), data)
+    return data
   })
 }
--- NodeJS/LF/mock-post-routes/static/app.js
+++ NodeJS/LF/mock-post-routes/static/app.js
@@ -1,11 +1,17 @@
 const API = 'http://localhost:3000'
 
-const populateProducts = async (category) => {
+const populateProducts = async (category, method = 'GET', payload) => {
   const products = document.querySelector('#products')
   products.innerHTML = ''
-  const res = await fetch(`${API}/${category}`)
+  const send =
+    method === 'GET'
+      ? {}
+      : {
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(payload),
+        }
+  const res = await fetch(`${API}/${category}`, { method, ...send })
   const data = await res.json()
-
   for (const product of data) {
     const item = document.createElement('product-item')
     for (const key of ['name', 'rrp', 'info']) {
@@ -19,9 +25,23 @@ const populateProducts = async (category) => {
 }
 
 const category = document.querySelector('#category')
+const add = document.querySelector('#add')
 
 category.addEventListener('input', async ({ target }) => {
+  add.style.display = 'block'
   await populateProducts(target.value)
+})
+
+add.addEventListener('submit', async (e) => {
+  e.preventDefault()
+  const { target } = e
+  const payload = {
+    name: target.name.value,
+    rrp: target.rrp.value,
+    info: target.info.value,
+  }
+  await populateProducts(category.value, 'POST', payload)
+  target.reset()
 })
 
 customElements.define(
