ba44b864091d3961e8f3e694f87c70a1a96a6261
wangeugene <yuzhen23@icloud.com>
2025-11-15 23:44:13 +0800

    feat: delete domain suffix line by domain name

---
+++ app/rule-updater/routes/delete/shadowrocket/index.js
@@ -0,0 +1,27 @@
+// routes/delete/shadowrocket/index.js
+// curl -X DELETE 'http://localhost:3000/delete/shadowrocket?domainName=eugene1.com'
+import { removeDomainRule } from "../../../config/shadowrocketConfig.js";
+import { readBackupText, writeBackupText, syncBackupToConfig, ensureBackupExists } from "../../../config/store.js";
+
+export default async function (fastify) {
+    fastify.delete("/", async (request, reply) => {
+        const { domainName } = request.query;
+        await ensureBackupExists();
+
+        console.log("[DELETE /delete/shadowrocket] domainName:", domainName);
+        if (!domainName) {
+            return reply.code(400).send({ error: "Missing domainName" });
+        }
+
+        const originalText = await readBackupText();
+        const hasCRLF = /\r\n/.test(originalText);
+        const EOL = hasCRLF ? "\r\n" : "\n";
+        let lines = originalText.split(/\r?\n/);
+
+        lines = await removeDomainRule(lines, domainName);
+        const updatedText = lines.join(EOL);
+        await writeBackupText(updatedText);
+        await syncBackupToConfig();
+        return { ok: true, deleted: domainName };
+    });
+}
