4023c997b802364f8ad1abc77e8fbf2124c998c2
wangeugene <yuzhen23@icloud.com>
2025-11-30 18:28:48 +0800

    learn: iOS official introduction doc

--- iOS/booking/booking/ViewController 2.swift
+++ iOS/booking/booking/ViewController 2.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class viewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ iOS/introduction/introduction/introductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  introductionApp.swift
+//  introduction
+//
+//  Created by euwang on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct introductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
