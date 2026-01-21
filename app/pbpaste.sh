56b8bbd4853a2e74cbaabcd8bf020ff7ecbb3857
wangeugene <yuzhen23@icloud.com>
2026-01-21 12:12:55 +0800

    test: quick commit & push

--- app/pbpaste.sh
+++ app/pbpaste.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+set -a
+source ./.env
+set +a
+
+if [[ -z "$REMOTE_HOST" ]]; then
+  echo "REMOTE_HOST is not set. Please set it in the .env file."
+  exit 1
+fi
+
 # Ask for a filename
 read -r -p "Please enter the filename you want to paste into the remote machine: " file_name
 
@@ -12,8 +21,8 @@ fi
 remote_path="/app/www/config/$file_name"
 
 echo "Pasting clipboard → $remote_path ..."
-pbpaste | ssh aws "cat > $remote_path"
+pbpaste | ssh $REMOTE_HOST "cat > $remote_path"
 
 echo "Done."
 echo "Last 3 lines of the remote file:"
-ssh aws "tail -n 3 $remote_path"
+ssh $REMOTE_HOST "tail -n 3 $remote_path"
