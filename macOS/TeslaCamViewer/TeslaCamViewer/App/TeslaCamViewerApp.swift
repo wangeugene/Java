0b6a6f5f8b96bb772f7046d1bb917e9be3a96114
wangeugene <yuzhen23@icloud.com>
2026-03-22 08:45:43 +0800

    macOS: clean structure for the very first start

---
+++ macOS/TeslaCamViewer/TeslaCamViewer/App/TeslaCamViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaCamViewerApp.swift
+//  TeslaCamViewer
+//
+//  Created by euwang on 3/21/26.
+//
+
+import SwiftUI
+
+@main
+struct TeslaCamViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ macOS/TeslaCamViewer/TeslaCamViewer/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TeslaCamViewer
+//
+//  Created by euwang on 3/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
